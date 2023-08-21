cb974c004dd31f1f50b7a1982429d7a2ebb0a0e8
Nathan Ramli <hezkyanatanael84@gmail.com>
2023-08-21 22:46:09 +0700

    fix: word size params

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,29 @@ use sui_types::crypto::SignatureScheme;
 
 fn main() {
     let args = env::args().skip(1).collect::<Vec<String>>();
+    let mut args = args.into_iter();
 
     let mut handle_vec = vec![];
 
+    let mut prefix = args
+        .next()
+        .unwrap_or_else(|| {
+            panic!("should define a prefix!");
+        })
+        .to_owned();
+    prefix.insert_str(0, "0x");
+
+    let word_size = args.next().unwrap_or("24".to_string());
+
     for _i in 0..20 {
-        let mut prefix: String = args
-            .get(0)
-            .unwrap_or_else(|| {
-                panic!("should define a prefix!");
-            })
-            .to_owned();
-        prefix.insert_str(0, "0x");
-        let word_size = args.get(1).unwrap_or(&"24".to_string()).to_owned().as_str();
+        let prefix = prefix.clone();
+        let word_size = word_size.clone();
 
         let handle = thread::spawn(move || loop {
             let (sui_address, _, _, mnemonic) = generate_new_key(
                 SignatureScheme::ED25519,
                 None,
-                Some("word".to_string().push_str(word_size)),
+                Some(format!("word{}", word_size)),
             )
             .unwrap();
 
